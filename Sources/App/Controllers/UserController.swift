bd57d707c57d1771dc57403d1796acb32bc5ffd7
Gonzalo Reyes Huertas <gonzalorehu@gmail.com>
2020-10-11 15:56:00 -0500

    Feature: Added user's list feature. Added validation so one user cannot see other user's ideas.

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -10,9 +10,16 @@ struct UserController: RouteCollection {
             .grouped(User.authenticator())
             .grouped(User.guardMiddleware())
             .post("login", use: loginHandler(_:))
+        // MARK: Ideas
+        let protected = group
+            .grouped("users")
+            .grouped(UserToken.authenticator())
+            .grouped(UserToken.guardMiddleware())
+        // 1. list all the user's ideas
+        protected.get(":userId", "ideas", use: getAllIdeasHandler(_:))
     }
     
-    // MARK: - Handlers
+    // MARK: - Auth handlers
     
     func loginHandler(_ req: Request) throws -> EventLoopFuture<UserToken> {
         // get the user
@@ -23,6 +30,23 @@ struct UserController: RouteCollection {
         return token
             .save(on: req.db)
             .map { token }
+    }
+    
+    // MARK: - Idea handlers
+    
+    func getAllIdeasHandler(_ req: Request) throws -> EventLoopFuture<[Idea]> {
+        // get the passed id
+        let id: UUID? = req.parameters.get("userId")
+        // make sure the token belongs to the user we are accessing
+        let auth = try req.auth.require(UserToken.self)
+        if auth.user.id != id {
+            return req.eventLoop.makeFailedFuture(Abort(.unauthorized))
+        }
+        // get the user's ideas
+        return User
+            .find(req.parameters.get("userId"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap({ user in user.$ideas.get(on: req.db) })
     }
     
 }
